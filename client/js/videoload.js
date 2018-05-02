4d646845d27ce544f399bfbc30f4fdd74c710374
GFdevelop <17930457+GFdevelop@users.noreply.github.com>
2018-05-02 16:19:11 +0200

    load video with query url now work

--- client/js/videoload.js
+++ client/js/videoload.js
@@ -6,14 +6,11 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
-var videoId = function() {
-  var youtubeGetId = 'sWEOoenLWNE'; // TODO: cambiare id statico con l'ID dell'URL
-  return youtubeGetId;
-}
 
 function onYouTubeIframeAPIReady() {
+	// TODO: manage when url have no query ex: http://gabriele.fulgaro.tw.cs.unibo.it/videopage.html
   player = new YT.Player('player', {
-    videoId: videoId(),
+    videoId: new URLSearchParams(window.location.search).get('videoId'),
     events: {
       'onReady': onPlayerReady,
     },
