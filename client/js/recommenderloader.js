f46ffa774fc8048dffebce1898ab6052e1763faf
Francesco Fornari <forno96@gmail.com>
2018-05-04 13:22:49 +0200

    Added border interaction and shadow on recommender

--- client/js/recommenderloader.js
+++ client/js/recommenderloader.js
@@ -25,48 +25,63 @@ function singleFrame(da, urlVid, i) {
   prsObj = new Object();
   var frame = '' +
   '<div class="col-4 pb-3">' +
-    '<div class="border border-light rounded-top">' +
-      '<h6 class = "text-right">'
+    '<div class="shadow rounded-top sFrm border">' +
+      '<h6 class = "text-right p-1">'
         if (da[i] != undefined) {frame += da[i]}
         else {frame += 'Dal Recommender'}
       frame += '</h6>' +
       '<div id="carousel' + i + '" class="carousel slide" data-ride="carousel">' +
-      '<ol class="carousel-indicators">'
-        for (var e = 0; e < urlVid[i].length; e++) {
-          frame += '<li data-target="#carousel' + i + '" data-slide-to="' + e + '"'
-          if (e == 0) {frame += 'class="active"'}
-          frame += '></li>'
-        }
-      frame += '</ol>' +
-      '<div class="carousel-inner">'
-        for (e = 0; e < (urlVid[i]).length; e++) {
-          frame += '<div class=\"carousel-item '
-          if (e == 0) {frame += 'active'}
-          prsObj.videoId = urlVid[i][e];
-          //console.log(prsObj);
-          frame += '">' +
-          '<a href="./videopage.html?' + $.param(prsObj) +'" style="text-decoration:none;">' +
-            '<div class="d-none d-md-block text-left text-muted pt-1 pl-1 pr-1 pb-0 mb-0">' +
-              '<h5>' + urlVid[i][e] + '</h5>' + // TODO: prendere gruppo e titolo attraverso i'id
-              '<h5> <small>' + urlVid[i][e] + '</small> </h5>' +
-            '</div>' +
-            '<img class="d-block w-100" src="https://i.ytimg.com/vi/'
-            frame += urlVid[i][e] // TODO: Assolutamente da cambiare
-            frame += '/mqdefault.jpg">' +
-          '</a>' +
-        '</div>'
-        }
-      frame += '</div>' +
-    '</div>' +
-    /*
-    '<a class="carousel-control-prev" href="#carousel' + i +
-    '" role="button" data-slide="prev"><span class="carousel-control-prev-icon"' +
-    'aria-hidden="true"></span><span class="sr-only">Previous</span></a>' +
-    '<a class="carousel-control-next" href="#carousel' + i +
-    '" role="button" data-slide="next"><span class="carousel-control-next-icon"' +
-    'aria-hidden="true"></span><span class="sr-only">Next</span></a>' +
-    */
+        '<ol class="carousel-indicators">'
+          for (var e = 0; e < urlVid[i].length; e++) {
+            frame += '<li data-target="#carousel' + i + '" data-slide-to="' + e + '"'
+            if (e == 0) {frame += 'class="active"'}
+            frame += '></li>'
+          }
+        frame += '</ol>' +
+        '<div class="carousel-inner">'
+          for (e = 0; e < (urlVid[i]).length; e++) {
+            frame += '<div class=\"carousel-item '
+            if (e == 0) {frame += 'active'}
+            prsObj.videoId = urlVid[i][e];
+            //console.log(prsObj);
+            frame += '">' +
+            '<a href="./videopage.html?' + $.param(prsObj) +'" style="text-decoration:none;">' +
+              '<div class="d-none d-md-block text-left text-muted pt-1 pl-1 pr-1 pb-0 mb-0">' +
+                '<h5>' + urlVid[i][e] + '</h5>' + // TODO: prendere gruppo e titolo attraverso i'id
+                '<h5> <small>' + urlVid[i][e] + '</small> </h5>' +
+              '</div>' +
+              '<div>' +
+                '<img class="d-block w-100" src="https://i.ytimg.com/vi/'
+                frame += urlVid[i][e] // TODO: Assolutamente da cambiare
+                frame += '/mqdefault.jpg">' +
+                '<a class="carousel-control-prev" href="#carousel' + i + // TODO: O trovare un modo per certrare le freccie o eliminarle
+                '" role="button" data-slide="prev"><span class="carousel-control-prev-icon"' +
+                'aria-hidden="true"></span><span class="sr-only">Previous</span></a>' +
+                '<a class="carousel-control-next" href="#carousel' + i +
+                '" role="button" data-slide="next"><span class="carousel-control-next-icon"' +
+                'aria-hidden="true"></span><span class="sr-only">Next</span></a>' +
+              '</div>' +
+            '</a>' +
+          '</div>'
+          }
+        frame += '</div>' +
+      '</div>' +
     '</div>' +
   '</div>'
   return frame;
 }
+
+$(document).ready(function(){
+    $("div.sFrm").mouseover(function(){
+        //$(this).css("border", "1px solid #b9b9b9");
+        $(this).addClass("border-secondary");
+    });
+    $("div.sFrm").mouseout(function(){
+        //$(this).css("border", "1px solid #e7e7e7");
+        $(this).removeClass("border-secondary");
+    });
+});
+
+$('.carousel').carousel({
+  interval: 2000
+})
