8f5cd3cf2f51da39092d329bd9d04282b0edb62f
Francesco Fornari <forno96@gmail.com>
2018-05-12 16:19:57 +0200

    Fixed arrow recommender and general recommender feeling

--- client/js/recommenderloader.js
+++ client/js/recommenderloader.js
@@ -180,7 +180,8 @@ function recommenderLoader() {
 
 function singleFrame(da, rec) {
   var frame = '' +
-    '<div id="carousel' + counter + '" class="carousel slide" data-ride="carousel">' +
+  '<div class="card sFrm">' +
+    '<div id="carousel' + counter + '" class="carousel slide " data-ride="carousel">' + /* col-6 col-md-4 col-lg-3*/
       '<ol class="carousel-indicators">'
         for (var e = 0; e < rec.length; e++) {
           frame += '<li data-target="#carousel' + counter + '" data-slide-to="' + e + '"'; if(e == 0){frame += 'class="active"'}; frame += '></li>'
@@ -190,25 +191,27 @@ function singleFrame(da, rec) {
         for (e = 0; e < rec.length; e++) {
         frame += '<div class=\"carousel-item '; if (e == 0) {frame += 'active'}; frame += '">' +
           '<a href="#?' + $.param(rec[e]) +'" style="text-decoration:none;">' +
-            '<div class="card sFrm">' +
-              '<div class="card-body text-secondary">' +
-                '<h5 class="card-title text-right text-dark font-weight-bold">'
+            '<div class="card-body text-secondary p-2">' +
+              '<h5 class="card-title text-right text-dark font-weight-bold">'
                 if (da != undefined) {frame += da} else {frame += 'Dal Recommender'};
-                frame += '<h5 class="card-title">' + rec[e].artist + '</h5>' +
-                '<p class="card-text">' + rec[e].title + '</p>' +
-              '</div>' +
-              '<img class="card-img-bottom" src="https://i.ytimg.com/vi/' + rec[e].videoID + '/mqdefault.jpg" alt="Card image cap">' +
+              frame += '</h5>' +
+              '<p class="card-text mb-1 h6">' + rec[e].artist + '</p>' +
+              '<p class="card-text h6"><small class="text-muted">' + rec[e].title + '</small></p>' +
             '</div>' +
+            '<img class="card-img-bottom" src="https://i.ytimg.com/vi/' + rec[e].videoID + '/mqdefault.jpg" alt="Card image cap">' +
           '</a>' +
         '</div>'
         }
       frame += '</div>' +
-      '<a class="carousel-control-prev" href="#carousel' + counter + // TODO: O trovare un modo per certrare le freccie o eliminarle
-      '" role="button" data-slide="prev"><span class="carousel-control-prev-icon"' +
-      'aria-hidden="true"></span><span class="sr-only">Previous</span></a>' +
-      '<a class="carousel-control-next" href="#carousel' + counter +
-      '" role="button" data-slide="next"><span class="carousel-control-next-icon"' +
-      'aria-hidden="true"></span><span class="sr-only">Next</span></a>' +
+      '<a class="carousel-control-prev mt-5" href="#carousel' + counter + '" role="button" data-slide="prev">' +
+        '<span class="carousel-control-prev-icon mt-5" aria-hidden="true"></span>' +
+        '<span class="sr-only">Previous</span>' +
+      '</a>' +
+      '<a class="carousel-control-next mt-5" href="#carousel' + counter + '" role="button" data-slide="next">' +
+        '<span class="carousel-control-next-icon mt-5" aria-hidden="true"></span>' +
+        '<span class="sr-only">Next</span>' +
+      '</a>' +
+    '</div>' +
   '</div>'
   counter = counter + 1;
   return frame;
