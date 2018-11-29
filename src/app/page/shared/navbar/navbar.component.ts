22814eb4245738785bacc256f525b173b9c9911e
UniPolv <mattia.polverini@studio.unibo.it>
2018-11-29 14:51:43 +0100

    Renamed ./app/shared/ into ./app/services

--- src/app/page/shared/navbar/navbar.component.ts
+++ src/app/page/shared/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { SearchService } from '../../../shared/search/search.service';
+import { SearchService } from '../../../services/search/search.service';
 
 @Component({
   selector: 'app-navbar',
