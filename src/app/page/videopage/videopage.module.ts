1c13c4ed27e2a31595e48244063826fd77bbc5b1
Francesco Fornari <forno96@gmail.com>
2018-12-10 21:00:27 +0100

    Color setting + Recommender

--- src/app/page/videopage/videopage.module.ts
+++ src/app/page/videopage/videopage.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { VideopageComponent } from './videopage.component';
 import { RecommenderComponent } from './recommender/recommender.component';
@@ -13,8 +14,10 @@ import { WikiboxComponent } from './wikibox/wikibox.component';
   declarations: [VideopageComponent, RecommenderComponent, PlayerComponent, WikiboxComponent],
   imports: [
 	TabsModule.forRoot(),
+    CarouselModule.forRoot(),
     CommonModule,
     RouterModule
   ]
 })
+
 export class VideopageModule { }
