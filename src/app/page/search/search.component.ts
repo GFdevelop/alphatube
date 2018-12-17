cc6237f1e714f89262c93a3b99f6c8b214810c61
GFdevelop <gabriele.fulgaro@gmail.com>
2018-12-17 13:36:17 +0100

    Testing search results loader

--- src/app/page/search/search.component.ts
+++ src/app/page/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { YoutubeService } from '../../services/youtube/youtube.service';
@@ -11,7 +11,8 @@ import { YoutubeService } from '../../services/youtube/youtube.service';
 export class SearchComponent implements OnInit {
 
   searchResults: any;
-  q: string;
+  params: any;
+  loading = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -19,16 +20,40 @@ export class SearchComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.route.params.subscribe((params) => {
-      this.q = params.q;
-      this.yt.getSearch('q', params.q).subscribe(
-        (data: any) => {
-          this.searchResults = data;
-          // ~ console.log(this.searchResults);
-        },
-        error => console.log(error)
-        );
+    this.route.params.subscribe((urlParams) => {
+      this.searchResults = null;
+      this.params = {q: urlParams.q};
+      this.search(this.params);
     });
   }
 
+  search(opt: any) {
+    this.yt.getSearch(opt).subscribe(
+      (data: any) => {
+        if (this.searchResults) {
+          this.searchResults.nextPageToken = data.nextPageToken;
+          this.searchResults.items = this.searchResults.items.concat(data.items);
+        } else {
+          this.searchResults = data;
+        }
+        this.loading = false;
+      },
+      error => console.log(error)
+    );
+  }
+
+  @ViewChild('list') list: ElementRef;
+
+  @HostListener('window:scroll', ['$event']) onWindowScroll() {
+    if (this.searchResults.nextPageToken) {
+      if ((window.innerHeight + window.scrollY) >= this.list.nativeElement.children[this.searchResults.items.length - 3].offsetTop) {  // visible height + pixel scrolled >= total height
+        if (this.loading == false) {
+          this.loading = true;
+        this.params = {...this.params, ...{pageToken: this.searchResults.nextPageToken} };
+        this.search(this.params);
+    // ~ console.log(this.list.nativeElement.children[this.searchResults.items.length - 3].offsetTop);
+  }
+      }
+    }
+  }
 }
