bc457549c538e084f256f0f367b40eb897ca8e93
Francesco Fornari <forno96@gmail.com>
2018-12-19 16:26:44 +0100

    Recommender auto generated

--- src/app/page/videopage/recommender/recommender.component.ts
+++ src/app/page/videopage/recommender/recommender.component.ts
@@ -12,8 +12,8 @@ import { YoutubeService } from '../../../services/youtube/youtube.service';
 })
 export class RecommenderComponent implements OnInit {
     show: boolean = true;
-    FVvideos: any;
-    YTvideosCor: any;
+    videoBuffer: any = ['inizialization'];
+    videoNameBuffer: any = ['inizialization'];
     YTvideosQue: any;
 
     query: any = 'test'; // TODO: da eliminare
@@ -25,13 +25,29 @@ export class RecommenderComponent implements OnInit {
     ) { }
 
     ngOnInit() {
+        this.videoBuffer.pop();
+        this.videoNameBuffer.pop();
         this.alphalistService.getAll().subscribe(
-          (data: any) => { this.FVvideos = data; },
+          (data: any) => {
+              var FVvideos = data;
+              for (var i = 0; i < 10; i++) {
+                  var j = Math.floor(Math.random() * (FVvideos.length + 1));
+                  var tmp = FVvideos[i];
+                  FVvideos[i] = FVvideos[j];
+                  FVvideos[j] = tmp;
+              }
+              this.videoBuffer.push(FVvideos);
+              this.videoNameBuffer.push('Fvitali');
+          },
           error => console.log(error)
         );
         this.route.params.subscribe( params => {
             this.youtubeService.getSearch({relatedToVideoId: params.videoId, maxResults: '11'}).subscribe(
-                (data: any) => {this.YTvideosCor = data.items;},
+                (data: any) => {
+                    var YTvideosCor = data.items;
+                    this.videoBuffer.push(YTvideosCor);
+                    this.videoNameBuffer.push('YouTube');
+                },
                 error => console.log(error)
             );
             this.youtubeService.getSearch({q: this.query, maxResults: '11'}).subscribe(
