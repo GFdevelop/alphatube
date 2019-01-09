08765dcf1113496d5593d6aa5f7c5621925feb56
AriannaAvoni <ariavoni19@gmail.com>
2019-01-09 16:19:35 +0100

    homepage random

--- src/app/services/youtube/youtube.service.ts
+++ src/app/services/youtube/youtube.service.ts
@@ -34,7 +34,7 @@ export class YoutubeService {
         params: {
           part: 'snippet,player,statistics',
           id: videoId,
-          fields: 'etag,items(etag,id,player,snippet(categoryId,description,publishedAt,tags,title),statistics(dislikeCount,likeCount,viewCount)),visitorId',
+          fields: 'etag,items(etag,id,player,snippet(categoryId,description,publishedAt,tags,title,thumbnails/medium/url),statistics(dislikeCount,likeCount,viewCount)),visitorId',
           key: this.devKey
         }
       }
