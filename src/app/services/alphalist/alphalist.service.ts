a99dd968d06846862d3d3ea4c451b8cc978edaca
Francesco Fornari <forno96@gmail.com>
2019-01-29 11:07:35 +0100

    FV works, added reason

--- src/app/services/alphalist/alphalist.service.ts
+++ src/app/services/alphalist/alphalist.service.ts
@@ -7,12 +7,16 @@ import { HttpClient } from '@angular/common/http';
 
 export class AlphalistService {
 
-  private URL = 'http://site1825.tw.cs.unibo.it/video.json';
+  private URL = 'http://site1825.tw.cs.unibo.it';
 
   constructor(private http: HttpClient) { }
 
   // ~ GET starting videos from the fvitali
   getAll() {
-    return this.http.get(this.URL);
+    return this.http.get(this.URL + '/video.json');
+  }
+
+  getGlobpop(videoId: string) {
+    return this.http.get(this.URL + '/TW/globpop?id=' + videoId);
   }
 }
