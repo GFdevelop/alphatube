685503759d80ca55311d1c6de4e16ba1fa64f808
AriannaAvoni <ariavoni19@gmail.com>
2019-02-10 16:51:38 +0100

    random
    recommender random, bozza about page

--- src/app/services/youtube/youtube.service.ts
+++ src/app/services/youtube/youtube.service.ts
@@ -72,4 +72,18 @@ export class YoutubeService {
       }
     );
   }
+
+  //richiesta al sito per una particolare playlist
+  //this.apiref è quello oche va da https a v3, quindi inseriremo dentro a params quello che ci serve
+  //torna 30 elementi della playlist
+   getPlaylist(playlistID: string) {
+     let params= {
+       part: 'snippet',
+       maxResults: '30',
+       playlistId: playlistID,
+       key: this.devKey
+     };
+     //part=snippet%2C+id&maxResults=30&playlistId=' + randomPlaylistId + '&key=' + API_KEY
+     return this.http.get(this.apiRef + '/playlistItems',{params})
+   }
 }
