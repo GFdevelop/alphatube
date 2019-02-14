8be4037dd2b1d808dcb7eb2138fa7c3889941368
Francesco Fornari <forno96@gmail.com>
2019-02-14 12:38:13 +0100

    merge fix

--- server/atlas.js
+++ server/atlas.js
@@ -1,26 +1,89 @@
 const express = require('express');
+	cors = require('cors');	// for cross origin resource sharing request
+	path = require('path');
+	jsonDB = require('node-json-db');
+	fs = require('fs');
 const port = 8000;
 var atlas = express();
-	cors = require('cors');
-	path = require('path');
 
+// ~ var origin = '*';
+
+// ~ if (fs.existsSync('./.dockerenv')) {
+	// ~ var origin = 'http://site1826.tw.cs.unibo.it';
+	// ~ var origin = 'http://gabriele.fulgaro.tw.cs.unibo.it';
+	// ~ var clientdir = '../alphatube';
+	// ~ var serverdir = './webapp/server';
+// ~ }
+// ~ else {
+	// ~ var origin = 'http://localhost:8000';
+	// ~ var clientdir = '../dist/alphatube';
+	// ~ var serverdir = './server';
+// ~ }
+
+// ~ var currentDir;
+// ~ fs.readdir('./webapp', function(err, items) {
+   	// ~ it = items;
+// ~ });
+	// ~ res.send(JSON.stringify(currentDir));		// put this in your method
+
+
+// CORS
 var corsOption = {
 	origin: 'http://site1826.tw.cs.unibo.it',
 	optionSuccessStatus: 200 //Legacy browser (IE 11) support
 }
 
-atlas.use(cors(corsOption));
-atlas.use(express.static(path.join(__dirname, '../alphatube'));
+// ~ atlas.use(cors(corsOption));
+atlas.use(cors());
+atlas.use(express.static(path.join(__dirname, '../alphatube')));
 
-atlas.get('*', (req, res) => {	
-	res.sendFile(path.join(__dirname, '../alphatube/index.html'));
-});
+
+//JSON-DB
+var db = new jsonDB("./webapp/server/db", true, true);
+
+var lastID;
+try {
+	lastID = db.getData("/lastID");
+} catch(error) {
+	lastID = 0;
+	db.push("/lastID", lastID);
+};
+
+// ~ var user = {
+	// ~ from: "video0",
+	// ~ to: "video4",
+	// ~ reason: "abc"
+// ~ }
+// ~ db.push("/3/list[]",user,true);
+
+
+//ROUTE
+// ~ atlas.options('*', cors(corsOption));
+atlas.options('*', cors());
 
 atlas.get('/globpop', function(req, res) {
-	res.send("Hello");
-	console.log("Ciao");
+	if (req.query.id) res.send(path);
+	else res.send(__dirname);
 });
 
+atlas.get('/crazy', function(req, res) {
+	try {
+		var uid = db.getData("/" + req.query.user);
+	} catch(error) {
+		lastID = lastID + 1;
+		db.push("/" + lastID, {list: []});
+		db.push("/lastID", lastID);
+		uid = lastID;
+	}
+	res.json({ id: uid });
+});
+
+atlas.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../alphatube/index.html'));
+});
+
+
+//START
 atlas.listen(port, () => {
   console.log('Server started at port ' + port + '!');
 });
